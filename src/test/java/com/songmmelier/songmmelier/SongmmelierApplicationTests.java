55b027dcc5d3c043851fbf9729f1f95b4ebd015c
Juhee <viimv2000@gmail.com>
2024-06-19 14:12:38 +0900

    Create

---
+++ src/test/java/com/songmmelier/songmmelier/SongmmelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songmmelier.songmmelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongmmelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
